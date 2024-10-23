b6d41d634980fedff9f81127db0a477d5255dab0
CharaD7 <jijakahn6@gmail.com>
2024-10-23 14:34:27 +0000

    Implemented basic counter using state rune

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,12 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+  let count = 0;
+
+  function increment() {
+    count += 1;
+  }
+</script>
+
+<button on:click={increment}>
+  Clicked {count}
+  { count === 1 ? 'time' : 'times' }
+</button>
