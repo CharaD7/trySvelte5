af81f6abc5993d62b39f4753b4f8f660f6ac7ffa
CharaD7 <jijakahn6@gmail.com>
2024-11-11 19:52:08 +0000

    Styled button and header

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -15,3 +15,30 @@
     ></button>
   {/each}
 </div>
+
+<style>
+  h1 {
+    transition: color 0.2s;
+  }
+
+  div {
+    display: grid;
+    grid-template-columns: repeat(7, 1fr);
+    gap: 5px;
+    max-width: 400px;
+  }
+
+  button {
+    aspect-ratio: 1;
+    border-radius: 50%;
+    background: var(--color, #fff);
+    transform: translate(-2px, -2px);
+    transition: all 0.1s;
+  }
+
+  button[aria-current="true"] {
+    transform: none;
+    filter: none;
+    box-shadow: inset 3px 3px 4px rgba(0, 0, 0, 0.2);
+  }
+</style>
