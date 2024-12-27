52584048bf8b518f3b6c2df1da0bd2806fa23f94
CharaD7 <jijakahn6@gmail.com>
2024-12-27 20:52:20 +0000

    Implemented each loop

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,44 +1,19 @@
 <script lang="ts">
-  const colors = ['red', 'blue', 'green', 'yellow', 'purple', 'orange', 'pink', 'brown', 'black', 'white'];
-  let selected = $state(colors[0]);
+  import Thing from './Thing.svelte';
+
+  let things = $state([
+    { id: 1, name: 'apple' },
+    { id: 2, name: 'banana' },
+    { id: 3, name: 'carrot' },
+    { id: 4, name: 'doughnut' },
+    { id: 5, name: 'eggplant' }
+  ]);
 </script>
 
-<h1 style="color: {selected}">Pick a colour</h1>
+<button onclick={() => things.shift()}>
+  Reomve first thing
+</button>
 
-<div>
-  {#each colors as color}
-    <button
-      style="background: {color}"
-      aria-label={color}
-      aria-current={selected === color}
-      onclick={() => selected = color}
-    ></button>
-  {/each}
-</div>
-
-<style>
-  h1 {
-    transition: color 0.2s;
-  }
-
-  div {
-    display: grid;
-    grid-template-columns: repeat(7, 1fr);
-    gap: 5px;
-    max-width: 400px;
-  }
-
-  button {
-    aspect-ratio: 1;
-    border-radius: 50%;
-    background: var(--color, #fff);
-    transform: translate(-2px, -2px);
-    transition: all 0.1s;
-  }
-
-  button[aria-current="true"] {
-    transform: none;
-    filter: none;
-    box-shadow: inset 3px 3px 4px rgba(0, 0, 0, 0.2);
-  }
-</style>
+{#each things as thing}
+  <Thing name={thing.name} />
+{/each}
