81e52f6ae5f9ab4e4b847456701897dfdd79422b
CharaD7 <jijakahn6@gmail.com>
2024-12-27 20:51:39 +0000

    Created Thing component

---
+++ src/routes/Thing.svelte
@@ -0,0 +1,17 @@
+<script>
+  const emojis = {
+    apple: '🍎',
+    banana: '🍌',
+    carrot: '🥕',
+    doughnut: '🍩',
+    eggplant: '🍆',
+  };
+
+  // name is updated whenever the prop value changes
+  let { name } = $props();
+
+  // ...but `emoji` is fixed upon initialisation
+  const emoji = emojis[name];
+</script>
+
+<p>{emoji} = {name}</p>
